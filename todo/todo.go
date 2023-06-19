d1e9c7528b414915a88e9bff0f9329a1b3d7c7fb
Jose Ignacio Martin <jimartinfdez@outlook.com>
2023-06-19 13:49:16 +0200

    Added read from file

--- todo/todo.go
+++ todo/todo.go
@@ -2,8 +2,7 @@ package todo
 
 import (
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
+	"os"
 )
 
 type Item struct {
@@ -16,12 +15,24 @@ func SaveItems(filename string, items []Item) error {
 		return err
 	}
 
-	fmt.Println(string(b))
-
-	err = ioutil.WriteFile(filename, b, 0644)
+	err = os.WriteFile(filename, b, 0644)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func ReadItems(filename string) ([]Item, error) {
+	b, err := os.ReadFile(filename)
+	if err != nil {
+		return []Item{}, err
+	}
+
+	var items []Item
+	if err := json.Unmarshal(b, &items); err != nil {
+		return []Item{}, err
+	}
+
+	return items, nil
 }
