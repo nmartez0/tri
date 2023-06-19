3bb5b2f85ff2c9c830f63e0efed017a47c902c1f
Jose Ignacio Martin <jimartinfdez@outlook.com>
2023-06-19 13:53:52 +0200

    Initial list command works

---
+++ cmd/list.go
@@ -0,0 +1,40 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/nmartez0/tri/todo"
+	"github.com/spf13/cobra"
+)
+
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List todo's entries",
+	Long:  `List a`,
+	Run: func(cmd *cobra.Command, args []string) {
+		items, err := todo.ReadItems("/home/nmartin/.tridos.json")
+		if err != nil {
+			log.Printf("%v", err)
+		}
+		fmt.Println(items)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
